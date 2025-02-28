5f3a8f2cb0df2d16fc246739738f9f654e99fe64
ilyas bural <ilyasbural@gmail.com>
2025-03-01 00:28:07 +0300

    restsharp library has been added to platform area

--- Universe.Platform/Program.cs
+++ Universe.Platform/Program.cs
@@ -5,7 +5,6 @@ using Universe.Platform;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
