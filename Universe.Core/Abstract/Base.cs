22e912b117b18d283855f5b21d985722f337430c
ilyas bural <ilyasbural@gmail.com>
2025-02-28 20:20:13 +0300

    base, businessobject classes added to project

---
+++ Universe.Core/Abstract/Base.cs
@@ -0,0 +1,10 @@
+﻿namespace Universa.Core
+{
+    public abstract class Base<T>
+    {
+        public Guid Id { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Universe.Core/Abstract/BusinessObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Universa.Core
+{
+    public abstract class BusinessObject<T>
+    {
+        protected T Data { get; set; } = default!;
+        protected List<T> Collection { get; set; } = null!;
+
+        public BusinessObject()
+        {
+
+        }
+    }
+}
