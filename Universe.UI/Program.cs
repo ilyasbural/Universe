ca7b0dc0fee0c54a038b9a97ec7e5784ab498c20
ilyas bural <ilyasbural@gmail.com>
2025-03-25 00:58:07 +0300

    bootstrap installed

--- Universe.UI/Program.cs
+++ Universe.UI/Program.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Universe.UI;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
