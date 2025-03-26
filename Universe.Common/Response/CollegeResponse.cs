4e031dbd68eb371c0e6c836ac3eb4deb4422c100
ilyas bural <ilyasbural@gmail.com>
2025-03-26 16:31:04 +0300

    changed collegeresponse

--- Universe.Common/Response/CollegeResponse.cs
+++ Universe.Common/Response/CollegeResponse.cs
@@ -3,13 +3,14 @@
 	public class CollegeResponse : Response<CollegeResponse>
 	{
 		public Guid Id { get; set; }
-        public string Name { get; set; } = String.Empty;
-        public DateTime RegisterDate { get; set; }
-        public DateTime UpdateDate { get; set; }
-        public bool IsActive { get; set; }
-        public CollegeResponse()
-        {
-            
-        }
-    }
+		public string Name { get; set; } = String.Empty;
+		public DateTime RegisterDate { get; set; }
+		public DateTime UpdateDate { get; set; }
+		public bool IsActive { get; set; }
+
+		public CollegeResponse()
+		{
+
+		}
+	}
 }
